26211d2f0368c5c5e89bc5ad7574861834591207
Maksim <menshikovma@yandex.ru>
2023-01-08 16:28:49 +0500

    timus

---
+++ src/timus/timus_task_1876/Main.java
@@ -0,0 +1,24 @@
+package timus.timus_task_1876;
+
+import java.io.*;
+import java.util.Locale;
+
+public class Main {
+    static StreamTokenizer input = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static final int nextInt() throws IOException {
+        input.nextToken();
+        return (int) input.nval;
+    }
+
+    public static void main(String str[]) throws IOException {
+        Locale.setDefault(Locale.US);
+
+        int a = nextInt() << 1;
+        int b = nextInt() << 1;
+
+        out.println(Math.max(a + 39, b + 40));
+        out.flush();
+    }
+}
---
+++ src/timus/timus_task_1924/Main.java
@@ -0,0 +1,26 @@
+package timus.timus_task_1924;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    //static StreamTokenizer input = new StreamTokenizer ( buffer );
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String str[]) throws IOException {
+        Locale.setDefault(Locale.US);
+
+        int n = Integer.parseInt(buffer.readLine());
+
+        if ((n * (n + 1) / 2) % 2 == 0) {
+            out.print("black");
+        } else {
+            out.print("grimy");
+        }
+        out.flush();
+    }
+}
