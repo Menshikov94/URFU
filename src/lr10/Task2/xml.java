f9f6213e797d123b1963aef56c146c81750e4ca0
Maksim <menshikovma@yandex.ru>
2023-04-02 15:05:44 +0500

    lr10.Task2

---
+++ src/lr10/Task2/xml.java
@@ -0,0 +1,10 @@
+package lr10.Task2;
+
+public class xml {
+
+    public static void main(String[] args) {
+        searchParseXml asdd = new searchParseXml();
+        asdd.searchDevice("192.168.1.2", "LTV");
+
+    }
+}
